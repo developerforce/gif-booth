e54adbdc66f0095f73b1736e30748e6bd912dd24
yuriy_chistyakov <yuchistyakov@gmail.com>
2020-08-15 00:29:55 +0300

    small cleaning

--- cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-  cloud_name: 'dzd5mddlm',
-  api_key: '476574941218556',
-  api_secret: 'MlQU5oeTAJJ9tH4RVWRctuGdHwo'
-});
-
-module.exports = cloudinary;
