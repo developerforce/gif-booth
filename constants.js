fef2adc4cef9249b527d7c9c833e2782d5865e3a
Clifton Campbell <clif@mimio.io>
2020-08-28 19:10:54 -0700

    rmv unused file

--- constants.js
+++
@@ -1,7 +0,0 @@
-// BEFORE MERGING LOOK HERE!!!!!!
-// this needs to be replaced when dev is over:
-// const s3URL = 'https://bucketeer-7dcba73d-2692-4191-be53-1b4e69bfff3d.s3.amazonaws.com/';
-
-module.exports = {
-  s3URL: 'https://gif-app-test.s3.us-east-2.amazonaws.com/',
-};
