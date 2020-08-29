4fb19ab33c68d0c2c93227770c3d98b136e1b9dd
yuriy_chistyakov <yuchistyakov@gmail.com>
2020-08-29 14:41:05 +0300

    rm unused packages

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import 'spinkit/css/spinkit.css';
 import download from 'downloadjs';
 import { Link } from 'react-router-dom';
 import './Create.css';
