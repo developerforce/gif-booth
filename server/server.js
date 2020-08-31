6013d5e609a0583281c0961ff36b894fef150c03
Clifton Campbell <clif@mimio.io>
2020-08-31 08:58:46 -0700

    add home to routes

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ const app = express();
 app.set('port', process.env.PORT || 3001);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('/home', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+  );
   app.get('/new-gif', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
   );
