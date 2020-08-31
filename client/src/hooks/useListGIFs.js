f0afafa935c9b3df0457c5f7a5e3a9c145700405
Clifton Campbell <clif@mimio.io>
2020-08-31 08:17:39 -0700

    avoid setting state after component unmounts

--- client/src/hooks/useListGIFs.js
+++ client/src/hooks/useListGIFs.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 
 const itemsPerPage = 20;
 
+let mounted;
 let loadingTimeout;
-
 export default function useListGIFs() {
   const [isInitializing, setIsInitializing] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
@@ -11,28 +11,36 @@ export default function useListGIFs() {
   const [page, setPageState] = useState(0);
 
   const gifCount = data.length;
-
   const pageCount = Math.ceil(gifCount / itemsPerPage);
+  const start = page * itemsPerPage;
+  const gifs = data.slice(start, start + itemsPerPage);
+
+  const cancelLoadingTimeout = () => {
+    if (loadingTimeout) clearTimeout(loadingTimeout);
+  };
 
   const setPage = (index) => {
-    if (loadingTimeout) clearTimeout(loadingTimeout);
+    cancelLoadingTimeout();
     if (!isLoading) setIsLoading(true);
     setPageState(index);
     // setting a delay on loading the images on incriment here so we don't spam img requests
     loadingTimeout = setTimeout(() => setIsLoading(false), 500);
   };
 
-  const start = page * itemsPerPage;
-  const gifs = data.slice(start, start + itemsPerPage);
-
   useEffect(() => {
+    mounted = true;
     listGifs();
+    return () => {
+      mounted = false;
+      cancelLoadingTimeout();
+    };
   }, []);
 
   const listGifs = async () => {
     try {
       const res = await fetch('/listGifs');
       const json = await res.json();
+      if (!mounted) return;
       setData(json);
       setIsLoading(false);
       setIsInitializing(false);
@@ -47,7 +55,7 @@ export default function useListGIFs() {
     gifs,
     pageCount,
     start: start + 1,
-    end: page < pageCount - 1 ? start + itemsPerPage : gifCount,
+    end: Math.min(start + itemsPerPage, gifCount),
     isLoading,
     isInitializing,
     setPage,
