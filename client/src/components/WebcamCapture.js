b768c8cb284c4b7e5edf30f0677311d509ead03d
Clifton Campbell <clif@mimio.io>
2020-09-01 14:58:44 -0700

    clean up webcam component, properly tear down streaming on unmount and stop capture

--- client/src/components/WebcamCapture.js
+++ client/src/components/WebcamCapture.js
@@ -1,104 +1,111 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
-import PropTypes from "prop-types";
-import Webcam from "react-webcam";
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import PropTypes from 'prop-types';
+import Webcam from 'react-webcam';
 
 const getAspectRatio = () =>
-  window?.screen?.orientation?.type?.includes("portrait") ? 0.75 : 1 + 1 / 3;
+  window?.screen?.orientation?.type?.includes('portrait') ? 0.75 : 1 + 1 / 3;
 
 const constraints = {
   video: true,
   audio: false,
 };
 
+const isMCRecording = (mediaRecorderRef) =>
+  mediaRecorderRef?.current?.state === 'recording';
+
+let stream;
 const WebcamStreamCapture = ({
-  handleError,
-  handleLoaded,
-  handleStopCapture,
+  className,
   isPlaying,
+  onCaptureReady,
+  onError,
+  onStopCapture,
 }) => {
   const webcamRef = useRef(null);
   const mediaRecorderRef = useRef(null);
-  const [capturing, setCapturing] = useState(false);
   const [aspectRatio, setAspectRatio] = useState(getAspectRatio());
 
-  const [recordedChunks, setRecordedChunks] = useState([]);
+  const startCapture = useCallback(() => {
+    if (isMCRecording(mediaRecorderRef)) return;
+    mediaRecorderRef.current.start();
+  }, [mediaRecorderRef]);
 
-  const handleDataAvailable = useCallback(
+  const stopCapture = useCallback(() => {
+    if (!isMCRecording(mediaRecorderRef) || !stream) return;
+    mediaRecorderRef.current.stop();
+    stream.getVideoTracks().forEach(function (track) {
+      track.stop();
+    });
+  }, [mediaRecorderRef]);
+
+  const onDataAvailable = useCallback(
     ({ data }) => {
-      if (data.size > 0) {
-        setRecordedChunks((prev) => prev.concat(data));
-      }
+      if (!data || data.size === 0) return;
+      const blob = new Blob([data], {
+        type: 'video/webm',
+      });
+      onStopCapture(blob);
     },
-    [setRecordedChunks]
+    [onStopCapture]
   );
 
-  const handleStartCapture = useCallback(() => {
-    setCapturing(true);
-    mediaRecorderRef.current.start();
-  }, [setCapturing, mediaRecorderRef]);
-
-  const handleStopCaptureClick = useCallback(() => {
-    if (mediaRecorderRef.current instanceof MediaRecorder)
-      mediaRecorderRef.current.stop();
-    setCapturing(false);
-  }, [mediaRecorderRef, setCapturing]);
-
   const prepareMediaRecorder = async () => {
-    await navigator.mediaDevices.getUserMedia(constraints);
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-      mimeType: "video/webm",
+      mimeType: 'video/webm',
     });
-    mediaRecorderRef.current.addEventListener(
-      "dataavailable",
-      handleDataAvailable
-    );
-    handleLoaded();
+    mediaRecorderRef.current.addEventListener('dataavailable', onDataAvailable);
+    onCaptureReady();
   };
 
   useEffect(() => {
-    if (recordedChunks.length) {
-      const blob = new Blob(recordedChunks, {
-        type: "video/webm",
-      });
-      handleStopCapture(blob);
-      setRecordedChunks([]);
-    }
-  }, [recordedChunks, handleStopCapture]);
-
-  useEffect(() => {
-    if (isPlaying && !capturing) handleStartCapture();
-    if (!isPlaying && capturing) handleStopCaptureClick();
-  }, [isPlaying, capturing, handleStartCapture, handleStopCaptureClick]);
+    const isRecording = isMCRecording(mediaRecorderRef);
+    if (isPlaying && !isRecording) startCapture();
+    if (!isPlaying && isRecording) stopCapture();
+  }, [isPlaying, startCapture, stopCapture]);
 
   useEffect(() => {
     const applyAspectRatio = () => setAspectRatio(getAspectRatio());
-    window.addEventListener("orientationchange", applyAspectRatio);
-    // window.screen.orientation.lock('portrait-primary').catch(() => {});
-
+    window.addEventListener('orientationchange', applyAspectRatio);
     prepareMediaRecorder();
-
     return () => {
-      window.removeEventListener("orientationchange", applyAspectRatio);
-      // window.screen.orientation.unlock();
+      // tear everything down
+      window.removeEventListener('orientationchange', applyAspectRatio);
+      if (mediaRecorderRef?.current)
+        mediaRecorderRef.current.removeEventListener(
+          'dataavailable',
+          onDataAvailable
+        );
+      if (isMCRecording(mediaRecorderRef)) stopCapture();
     };
   }, []);
 
+  console.log(window.screen);
+
   return (
-    <Webcam
-      className="gif-video"
-      audio={false}
-      ref={webcamRef}
-      onUserMediaError={handleError}
-      videoConstraints={{ aspectRatio }}
-    />
+    <>
+      <Webcam
+        className={className}
+        audio={false}
+        ref={webcamRef}
+        onUserMediaError={onError}
+        videoConstraints={{ aspectRatio, facingMode: 'user' }}
+      />
+      {window?.screen?.orientation?.type}
+    </>
   );
 };
 
 WebcamStreamCapture.propTypes = {
-  handleError: PropTypes.func.isRequired,
-  handleStopCapture: PropTypes.func.isRequired,
+  className: PropTypes.string,
   isPlaying: PropTypes.bool.isRequired,
-  handleLoaded: PropTypes.func.isRequired,
+  onCaptureReady: PropTypes.func.isRequired,
+  onError: PropTypes.func.isRequired,
+  onStopCapture: PropTypes.func.isRequired,
+};
+
+WebcamStreamCapture.defaultProps = {
+  className: '',
 };
 
 export default WebcamStreamCapture;
